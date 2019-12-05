aa861fe03c41e8e5e3b72db83ece8dab5f7b6448
Stealthness <stevewestinlondon@gmail.com>
2019-12-05 02:43:46 +0000

    uploading bbk work

--- symbolic-regression/deap_symbolic_regression_example_2_solving_sin.py
+++ symbolic-regression/deap_symbolic_regression_example_2_solving_sin.py
@@ -1,45 +1,54 @@
-import math, operator, random
+import math
+import operator
+import random
 import matplotlib.pyplot as plt
 import numpy as np
 from deap import algorithms, base, creator, tools, gp
 
-# Set Parameters
+# Set Parameters (default values in comments)
+# MAX_GEN = 100
+# CXPB = 0.5
+# MUTPB = 0.1
+# MAX_VALUE = 17
+
 MAX_GEN = 100
 CXPB = 0.5
 MUTPB = 0.1
-MAX_VALUE =  25  # 17
-points = [math.pi*x/10 for x in range(-10,10)]
+MAX_VALUE = 25
+POINTS = [math.pi*x/10 for x in range(-10, 10)]
 ADJ = 0.1*math.pi
-random.seed(98)
+random.seed(318)
 
 # Set function want to find
 
-exp_function = lambda x: math.sin(x)
+# Defince the expected function
+def exp_function(x):
+    return math.sin(x)
+
 
 # Define new functions
-def protectedDiv(left, right):
+def protected_div(left, right):
     try:
         return left / right
     except ZeroDivisionError:
         return 1
 
-# Set our constants(terminals) and primatives(functions) set
 
+# Set our constants(terminals) and primitives(functions) set
 pset = gp.PrimitiveSet("MAIN", 1)
-
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(protectedDiv, 2, name="div")
+pset.addPrimitive(protected_div, 2, name="div")
 pset.addPrimitive(operator.neg, 1)
-pset.addEphemeralConstant("rand101", lambda: math.factorial(random.randint(0,7)))
+pset.addEphemeralConstant("randfactorial", lambda: math.factorial(random.randint(0, 7)))
 pset.renameArguments(ARG0='x')
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
-toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_= 4)
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=4)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
@@ -52,7 +61,8 @@ def evalSymbReg(individual, points):
     sqr_errors = ((evl_func(x) - exp_function(x))**2 for x in points)
     return math.fsum(sqr_errors) / len(points),
 
-toolbox.register("evaluate", evalSymbReg, points=points)
+
+toolbox.register("evaluate", evalSymbReg, points=POINTS)
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
@@ -62,10 +72,11 @@ toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_v
 toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=MAX_VALUE))
 
 print('start')
-random.seed(318)
 
 pop = toolbox.population(n=300)
 hof = tools.HallOfFame(1)
+
+# creating mstats to record evolution data
 
 stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
 stats_size = tools.Statistics(len)
@@ -74,20 +85,22 @@ mstats.register("avg", np.mean)
 mstats.register("std", np.std)
 mstats.register("min", np.min)
 mstats.register("max", np.max)
+
+# print log
 pop, log = algorithms.eaSimple(pop, toolbox, CXPB, MUTPB, MAX_GEN, stats=mstats, halloffame=hof, verbose=True)
 
+print("\nBest expression found is;")
 print(hof[0])
 
-
-f_points = [exp_function(x) for x in points]
-plt.plot(points, f_points , label='expected function')
+# creating graph using matplotlib
+exp_func_points = [exp_function(x) for x in POINTS]
+plt.plot(POINTS, exp_func_points, label='Expected function')
 
 evl_func = toolbox.compile(expr=hof[0])
-f_points  = [evl_func(x) for x in points]
-plt.plot(points, f_points , label='evluated function')
+eval_func_points  = [evl_func(x) for x in POINTS]
+plt.plot(POINTS, eval_func_points , label='Evaluated function')
 plt.legend()
 plt.plot()
-
 plt.show()
-# print log
+
 print('end')
