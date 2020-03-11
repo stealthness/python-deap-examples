e621363d1b134fe79ff16b5512a270e8e19781ae
Stealthness <stevewestinlondon@gmail.com>
2020-03-11 20:32:11 +0000

    tidy up script

--- ga_simple_rocket/script_for_single_rocket_reply.py
+++ ga_simple_rocket/script_for_single_rocket_reply.py
@@ -1,27 +1,34 @@
 """
-Purpose of this file is to separate the visual from ga algoritm.
-This is to be done by passing data of the chages of position and replaying them visually
+Purpose of this test script is to create a single individual rocket that will free fall and crash.
+A Bunch object is created to store the {time, ds, pos, vel, acc, has_landed, has_failed} details at each time interval
 """
-from graphics import Point
-
 from sklearn.utils import Bunch
 
-from ga_simple_rocket.functions import reply_single_rocket, get_rocket_shape
+from ga_simple_rocket.config import MAX_TIME_INTERVALS
+from ga_simple_rocket.functions import reply_single_rocket
 from ga_simple_rocket.individual_class import Individual
-from ga_simple_rocket.simple_rocket_class import SimpleRocket
 
+# creating the bunch object
 d = Bunch()
 d.name = 'r0'
 d.data = []
 
-ind = Individual('r0')
-ind.rocket
-ind.rocket.pos = 200
-ind.rocket.engine_force = 10
-ind.rocket.engine_on = False
-d.data.append({'time': 0, 'ds': 0.0, 'pos': ind.rocket.pos, 'failed': False, 'landed': False})
-for t in range(120):
-    ds, _ = ind.update(t)
-    d.data.append({'time': t+1, 'ds': ds, 'pos': ind.rocket.pos, 'failed': ind.rocket.has_failed, 'landed': ind.rocket.has_landed})
+# creating an individual and initial rocket data
+individual = Individual('r0')
+individual.rocket.pos = 200
+individual.rocket.engine_force = 10
+individual.rocket.engine_on = False
+info = {'time': 0, 'ds': 0.0, 'pos': individual.rocket.pos, 'vel': individual.rocket.vel, 'acc': individual.rocket.acc}
+info.update({'failed': False, 'landed': False})
+d.data.append(info)
 
+# Run simulation of the rockets free fall
+for t in range(MAX_TIME_INTERVALS):
+    ds, _ = individual.update(t)
+
+    info = {'time': t+1, 'ds': ds, 'pos': individual.rocket.pos, 'vel': individual.rocket.vel, 'acc': individual.rocket.acc}
+    info.update({'failed': individual.rocket.has_failed, 'landed': individual.rocket.has_landed})
+    d.data.append(info)
+
+# visualise the results
 reply_single_rocket(d, 300)
