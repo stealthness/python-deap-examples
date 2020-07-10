09f447dcdc9f3ecd6b4c2768fe6e7a983400fc7a
Stealthness <stevewestinlondon@gmail.com>
2020-07-10 02:50:51 +0100

    refactoring

--- ga_simple_mouse/tests/test_mouse_class.py
+++ ga_simple_mouse/tests/test_mouse_class.py
@@ -9,29 +9,23 @@ class TestIntMouse(TestCase):
         self.mouse0 = IntMouse('m0')
 
     def test_initial_IntMouse_has_default_0_0(self):
-        self.assertEqual(0, self.mouse0.x, f'mouse x:{self.mouse0.x}')
-        self.assertEqual(0, self.mouse0.y, f'mouse y:{self.mouse0.y}')
         self.assert_mouse_position(0, 0, self.mouse0.get_pos(), msg='test mouse at initial position')
 
     def test_m0_move_to_the_left_by_1(self):
         self.mouse0.move('left')
-        self.assertEqual(-1, self.mouse0.x, f'mouse x:{self.mouse0.x}')
-        self.assertEqual(0, self.mouse0.y, f'mouse y:{self.mouse0.y}')
+        self.assert_mouse_position(-1, 0, self.mouse0.get_pos(), msg='move left')
 
     def test_m0_move_to_the_right_by_1(self):
         self.mouse0.move('right')
-        self.assertEqual(1, self.mouse0.x, f'mouse x:{self.mouse0.x}')
-        self.assertEqual(0, self.mouse0.y, f'mouse y:{self.mouse0.y}')
+        self.assert_mouse_position(1, 0, self.mouse0.get_pos(), msg='move right')
 
     def test_m0_move_to_the_up_by_1(self):
         self.mouse0.move('up')
-        self.assertEqual(0, self.mouse0.x, f'mouse x:{self.mouse0.x}')
-        self.assertEqual(1, self.mouse0.y, f'mouse y:{self.mouse0.y}')
+        self.assert_mouse_position(0, 1, self.mouse0.get_pos(), msg='move up')
 
     def test_m0_move_to_the_down_by_1(self):
         self.mouse0.move('down')
-        self.assertEqual(0, self.mouse0.x, f'mouse x:{self.mouse0.x}')
-        self.assertEqual(-1, self.mouse0.y, f'mouse y:{self.mouse0.y}')
+        self.assert_mouse_position(0, -1, self.mouse0.get_pos(), msg='move down')
 
     # misc test
 
